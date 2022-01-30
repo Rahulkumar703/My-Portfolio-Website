a5f6e03a4499d82736830b0b81433f1c02c0d961
Devil Rk <rahulkumar703@outlook.com>
2022-01-30 15:26:42 +0530

    Compeleted Resopnsive work for Mobile in Navbar

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,23 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Style/Header.css'
 import './Style/Responsive-Header.css'
 
+
 function Header() {
+  const [activeTab, setActiveTab] = useState("Home");
   return (
     <header>
 
       <nav>
 
         <div className="title">
-          <a href="/" className="title-link">
+          <a href="#" className="title-link">
             <h1>My Potfolio</h1>
           </a>
         </div>
 
         <div className="nav-tabs">
           <ul className="nav-list">
-            <li href="/" className="nav-items">
-              <a href="/" className="nav-links" title="Home">
+            <li className="nav-items">
+              <a href="#"
+                className={`nav-links ${activeTab === "Home" ? "active" : ''}`}
+                title="Home"
+                onClick={() => { setActiveTab("Home") }}>
                 <span className="nav-links-icon">
                   <i className="fi fi-rr-home"></i>
                 </span>
@@ -26,8 +31,11 @@ function Header() {
                 </span>
               </a>
             </li>
-            <li href="/" className="nav-items">
-              <a href="/" className="nav-links" title="About">
+            <li className="nav-items">
+              <a href="#"
+                className={`nav-links ${activeTab === "About" ? "active" : ''}`}
+                title="About"
+                onClick={() => { setActiveTab("About") }}>
                 <span className="nav-links-icon">
                   <i className="fi fi-rr-info"></i>
                 </span>
@@ -36,8 +44,11 @@ function Header() {
                 </span>
               </a>
             </li>
-            <li href="/" className="nav-items">
-              <a href="/" className="nav-links" title="Projects" >
+            <li className="nav-items">
+              <a href="#"
+                className={`nav-links ${activeTab === "Projects" ? "active" : ''}`}
+                title="Projects"
+                onClick={() => { setActiveTab("Projects") }}>
                 <span className="nav-links-icon">
                   <i className="fi fi-rr-briefcase"></i>
                 </span>
@@ -46,8 +57,11 @@ function Header() {
                 </span>
               </a>
             </li>
-            <li href="/" className="nav-items">
-              <a href="/" className="nav-links" title="Contact" >
+            <li className="nav-items">
+              <a href="#"
+                className={`nav-links ${activeTab === "Contact" ? "active" : ''}`}
+                title="Contact"
+                onClick={() => { setActiveTab("Contact") }}>
                 <span className="nav-links-icon">
                   <i className="fi fi-rr-comment-alt"></i>
                 </span>
